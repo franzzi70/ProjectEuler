6fc766f4b4eae705f93f78074b42da884ed1157b
Franz Zoister <fzoister@yahoo.com>
2024-12-09 11:06:33 +0100

    ugly trial version, but gives solution.

--- P0138_SpecialIsoscelesTriangles/P0138_SpecialIsoscelesTriangles/main.cpp
+++ P0138_SpecialIsoscelesTriangles/P0138_SpecialIsoscelesTriangles/main.cpp
@@ -1,66 +1,174 @@
 
 #include <iostream>
 #include <chrono>
-#include "MyBigInteger.h"
+// #include "MyBigInteger.h"
+#include <intrin.h>
+#include <cstdint>
+
+#pragma intrinsic(_mul128)
 
 // ((b/2) ^ 2 + (b+1)^2 = c^2
 // ((b/2) ^ 2 + (b-1)^2 = c^2
 
  
- typedef MyBigInteger BigIntType;
 
-__int64 solve()
+
+class Int128
+{
+public:
+
+	Int128()
+	{
+		low = 0;
+		high = 0;
+	}
+
+	Int128(unsigned __int64 low, unsigned __int64 high)
+	{
+		this->low = low;
+		this->high = high;
+	}
+
+	
+	unsigned __int64 low;
+	unsigned __int64 high;
+
+	unsigned __int64 mul128(unsigned __int64 n1, unsigned __int64 n2)
+	{
+		low = _umul128(n1, n2, &high);
+		return low;
+	}
+
+	Int128 operator + (const Int128& other)
+	{
+		Int128 result;
+		result.low = low + other.low;
+		result.high = high + other.high;
+		if (result.low < low)
+		{
+			result.high += 1;
+		}
+		return result;
+	}
+
+	bool operator == (const Int128& other)
+	{
+		return low == other.low && high == other.high;
+	}
+
+	double sqrt()
+	{
+		return std::sqrt((double)*this);
+	}
+
+	operator double()
+	{
+		return ((unsigned __int64) low ) + high * ((double)UINT64_C(0x100000000)) * ((double)UINT64_C(0x100000000));
+	}
+};
+
+unsigned __int64 solve()
 {
 
-	BigIntType bhalf = 0;
-	BigIntType b = 0;
+	unsigned __int64 bhalf = 0;
+	unsigned __int64 b = 0;
 	bool done = false;
 	int foundCount = 0;
-	__int64 lSum = 0;
+	unsigned __int64 lSum = 0;
+	Int128 hSqr;
+	Int128 bhalfSqr;
+	Int128 lSqr;
+	Int128 LSqr;
+	unsigned __int64 lastB = 1;
+
 	while (!done)
 	{
-		if (b % 10000000 == BigIntType(0))
+		if (b % 10000000 == unsigned __int64(0))
 		{
 			std::cout << "b: " << b << std::endl;
 		}
 
-		bhalf += BigIntType(1);
+		bhalf += 1;
 		b += 2;
+
+		bool found = false;
 		for (__int32 alt = -1; alt <= 1; alt += 2)
 		{
-			BigIntType h = b + BigIntType(alt);
+			unsigned __int64 h = b + unsigned __int64(alt);
 
-			if (b == BigIntType(16))
+			if (b == unsigned __int64(16))
 				std::cout << "b: " << b << std::endl;
-			if (b == BigIntType(272))
+			if (b == unsigned __int64(272))
 			{
 				std::cout << "b: " << b << std::endl;
 			}
 
-			//__int64 lSqr = h * (__int64)h + bhalf * (__int64)bhalf;
-			BigIntType lSqr = h * h + bhalf * bhalf;
-			__int64 L = (int)round(sqrt(lSqr));
-			if (L * L == lSqr)
+			//unsigned __int64 lSqr = h * (unsigned __int64)h + bhalf * (unsigned __int64)bhalf;
+			(void)hSqr.mul128(h,h);
+			(void)bhalfSqr.mul128(bhalf,bhalf);
+			lSqr = hSqr + bhalfSqr;
+			//unsigned __int64 lSqr = h * h + bhalf * bhalf;
+			unsigned __int64 L = (unsigned __int64)round(lSqr.sqrt());
+			LSqr.mul128(L, L);
+			if (LSqr == lSqr)
 			{
-				std::cout << "b: " << (std::string)b << ", L: " << L << ", h: " << (std::string)h << std::endl;
+				found = true;
+				std::cout << "b: " << b << ", L: " << L << ", h: " << h << std::endl;
 				foundCount += 1;
 				lSum += L;
-				if (foundCount == 6)
+				if (foundCount == 12)
 				{
 					done = true;
 					break;
 				}
 			}
 		}
+		if (found)
+		{
+			double f;
+			//--double f = ((double)b) / double(lastB) - 1E-6;
+			if (b >= 272)
+			{
+				if (b==4896)
+					f = 17.94117;
+				else
+					if (b==87840)
+						f=17.9443;
+					else if (b==1576240)
+						f = 17.94425;
+					else
+						f=17.94426;
+
+				bhalf = (unsigned __int64)(bhalf * f);
+				b = bhalf * 2;
+			}
+
+			lastB = b;
+		}
+		//b = (unsigned __int64) (b * 1.118034);if 
+		//b = (unsigned __int64) (b * 17.944271);
+		//break;
 	}
 	return lSum;
 }
 
+void test()
+{
+	unsigned __int64 i1 = 1000000000000;
+	Int128 im1;
+	im1.mul128(i1, i1);
+	double d1 = (double)i1 * (double)i1;
+	double d2 = (double)im1;
+	unsigned __int64 sq = (unsigned __int64)round(im1.sqrt());
+}
 
 int main()
 {
+	//test();
+	//return 0;
+
 	auto t1 = std::chrono::high_resolution_clock::now();
-	__int64 solution = solve();
+	unsigned __int64 solution = solve();
 	auto t2 = std::chrono::high_resolution_clock::now();
 	auto microSec = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
 	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
