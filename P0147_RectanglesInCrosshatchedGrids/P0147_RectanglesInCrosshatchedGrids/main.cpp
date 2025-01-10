deb0e700801ed926e32dd685a812a811b7ca1e7e
Franz Zoister <fzoister@yahoo.com>
2025-01-10 19:17:55 +0100

    solved problem 147.

--- P0147_RectanglesInCrosshatchedGrids/P0147_RectanglesInCrosshatchedGrids/main.cpp
+++ P0147_RectanglesInCrosshatchedGrids/P0147_RectanglesInCrosshatchedGrids/main.cpp
@@ -2,15 +2,67 @@
 #include <iostream>
 #include <chrono>
 
-int getCount(int x, int y)
+
+int getCount(int x, int y, bool addSubCounts = true)
 {
-	int count = 0;
-	for (int h = 1; h <= y; h++)
+	int X = x;
+	int Y = y;
+	if (y > x)
 	{
-		for (int w = 1; w <= x; w++)
+		X = y;
+		Y = x;
+	}
+
+	int count = 0;
+	for (int w = 1; w <= X; w++)
+	{
+		for (int h = 1; h <= Y; h++)
 		{
-			count += (x - w + 1) * (y - h + 1);
+			int subCount = 0;
+			if (addSubCounts)
+			{
+				if (w < X || h < Y)
+				{
+					subCount = getCount(w, h, false);
+				}
+			}
+			count += (X - w + 1) * (Y - h + 1) + subCount;
 		}
+	}
+
+	;
+	// diagonal rectangles:
+	for (int w = 1; w <= 2 * X; w++)
+	{
+		bool foundForW = false;
+		for (int h = 1; h <= w; h++)
+		{
+			bool foundForH = false;
+			// CornerOnGrid
+			int w1 = 2 + (w - 1) / 2 + (h - 1) / 2;
+			int h1 = (1 + w + h) / 2;
+			if (w1 <= X && h1 <= Y)
+			{
+				int d1 = (X - w1 + 1) * (Y - h1 + 1);
+				count += d1 * (w == h ? 1 : 2);
+				foundForW = true;
+				foundForH = true;
+			}
+			// CornerOnCenter
+			int w2 = 1 + w / 2 + h / 2;
+			int h2 = (w + h + 2) / 2;	// hier weiter
+			if (w2 <= X && h2 <= Y)
+			{
+				int d2 = (X - w2 + 1) * (Y - h2 + 1);
+				count += d2 * (w == h ? 1 : 2);
+				foundForW = true;
+				foundForH = true;
+			}
+			if (!foundForH)
+				break; // no more rectangles possible with greater height
+		}
+		if (!foundForW) // no more rectangles possible with greater width
+			break;
 	}
 
 	return count;
@@ -18,9 +70,11 @@ int getCount(int x, int y)
 
 __int64 solve()
 {
-	int c1 = getCount(3, 2);
 
-	return 0;
+	//// test:
+	//int c1 = getCount(3, 2);
+
+	return getCount(47, 43);
 }
 
 int main()
