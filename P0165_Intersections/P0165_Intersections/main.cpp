19845791a7c80001395e9bf5c17da08df6478195
Franz Zoister <fzoister@yahoo.com>
2026-03-02 07:50:44 +0100

    move reservation out of critical section.

--- P0165_Intersections/P0165_Intersections/main.cpp
+++ P0165_Intersections/P0165_Intersections/main.cpp
@@ -274,8 +274,9 @@ void thread_body(rec_shared* shared)
 	int32_t thread_mod = shared->thread_mod;
 	shared->thread_mod += 1;
 	shared->candidates[thread_mod] = candidates;
-	candidates->reserve(((LINES_COUNT * LINES_COUNT) / 2) / thread_count);
 	shared->mtx.unlock();
+
+	candidates->reserve(((LINES_COUNT * LINES_COUNT) / 2) / thread_count);
 
 	int32_t i = 0;
 	int32_t j = thread_mod + 1;
